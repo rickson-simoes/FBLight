1811bd71f89fe4216c3eab9468b6fdc54f4ab875
Rickson <rickson.simoes@hotmail.com>
2019-12-05 17:13:09 -0300

    react configurado, eslint e editor config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "class-methods-use-this" : "off",
+    "no-param-reassign" : "off",
+    "camelcase" : "off",
+    "no-unused-vars" : ["error",{"argsIgnorePattern": "next"}],
+  },
+};
