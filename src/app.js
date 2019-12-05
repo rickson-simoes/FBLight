e95d0bc48974475e7661c2c6fbfde1ac123d396a
Rickson <rickson.simoes@hotmail.com>
2019-12-05 17:38:07 -0300

    primeiro componente

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function app() {
+  return <h1>retornando componente</h1>;
+}
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>teste</h1>, document.getElementById('app'));
+import App from './app';
+
+render(<App />, document.getElementById('app'));
